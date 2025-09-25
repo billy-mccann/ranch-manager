6bb781ae3bc2e45a4e4b91f6beb176ed19b0c814
Bill McCann <hyperfine262@gmail.com>
2025-09-25 11:34:45 -0700

    [UI] Creates cards on main view

--- RanchManager/ContentView.swift
+++ RanchManager/ContentView.swift
@@ -10,15 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("At a Glance")
+                .font(.title)
+                .padding([.bottom, .top], 80)
+            MainCardView(typeLabel: "All Cows", numLabel: "600")
+
+            HStack {
+                MainCardView(typeLabel: "Bulls", numLabel: "120")
+                MainCardView(typeLabel: "Cows", numLabel: "200")
+
+            }
+            HStack {
+                MainCardView(typeLabel: "Calves", numLabel: "200")
+                MainCardView(typeLabel: "Steers", numLabel: "80")
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct MainCardView: View {
+    var typeLabel: String
+    var numLabel: String
+    var body: some View {
+        VStack{
+            Text(typeLabel)
+            Text(numLabel)
+        }
+        .navigationDestination(for: Int.self) { num in
+            DetailView()
+        }
+        .padding()
+        .background(.blue.opacity(0.4))
+        .clipShape(.buttonBorder)
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        Text("Some number")
+    }
 }
