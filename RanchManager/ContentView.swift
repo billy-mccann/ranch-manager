dfcf3697ab12e9fe70ae22219f646079e0f85f56
Bill McCann <hyperfine262@gmail.com>
2025-09-25 11:03:05 -0700

    Initial Commit

---
+++ RanchManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RanchManager
+//
+//  Created by Bill McCann on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RanchManager/RanchManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RanchManagerApp.swift
+//  RanchManager
+//
+//  Created by Bill McCann on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct RanchManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
