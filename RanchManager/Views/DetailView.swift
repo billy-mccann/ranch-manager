3178ebc2783ec8ab978d31cd2d346acb73aa9c2f
Bill McCann <hyperfine262@gmail.com>
2025-10-09 17:04:50 -0700

    [DetailView] Adds placeholder for list.

--- RanchManager/Views/DetailView.swift
+++ RanchManager/Views/DetailView.swift
@@ -7,6 +7,19 @@ struct DetailView: View {
       Spacer()
       Text("The number of \(animalType.lowercased()) is: \(numberOfThings)")
       Spacer()
+      Text("<Table of all cows goes here>")
+      HStack{
+        Text("Betsy")
+        Text("River Ranch")
+        Text("DOB: 10-7-2021")
+      }
+      HStack{
+        Text("JuneBug")
+        Text("Forest Pasture")
+        Text("DOB: 04-13-2020")
+      }
+
+      Spacer()
       GoToAddCowButton()
       Spacer()
     }
